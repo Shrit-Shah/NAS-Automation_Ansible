2368258cee9b0f8ee08cb06aa877a585b5f7cbd2
Harshil Shah <shaharshil2001@gmail.com>
2021-10-03 22:20:36 +0530

    local commit

--- NAS.sh
+++ NAS.sh
@@ -269,7 +269,7 @@ ansible_setup()
     then
 	    sudo mkdir /etc/ansible/
     fi
-    echo -e "[defaults]\ninventory = /.NAS/.ip.txt\nhost_key_checking = False\ndeprecation_warnings = False\ncommand_warnings = False\n\n[privilege_escalation]\nbecome=True\nbecome_method=sudo\nbecome_user=root\nbecome_ask_pass=True" > /etc/ansible/ansible.cfg
+    echo -e "[defaults]\ninventory = /.NAS/.ip.txt\nhost_key_checking = False\ndeprecation_warnings = False\ncommand_warnings = False\n\n[privilege_escalation]\nbecome=True\nbecome_method=sudo\nbecome_user=root\nbecome_ask_pass=False" > /etc/ansible/ansible.cfg
 
     sudo dnf list installed | grep epel-release &>> /dev/null
     if [ $? -eq 1 ]
