5cf8dea784941a2ca6dc794064fa2217aaabaf45
Harshil Shah <shaharshil2001@gmail.com>
2021-10-03 23:29:10 +0530

    local commit

--- NAS.sh
+++ NAS.sh
@@ -109,7 +109,7 @@ new_setup()
                 echo -e "/${server_dir} ${client_ip}(rw,no_root_squash)" > /tmp/exports
                 spin2  "Configuring NAS server. Running ansible playbook  "  &
                 pid=$!
-                ansible-playbook nas-playbook.yml --extra-vars "server_bak_dir=${server_dir}" &>> /dev/null
+                ansible-playbook nas-playbook.yml -e "server_bak_dir=${server_dir}" &>> /dev/null
                 play_process=$?
                 echo -e "\n"
                 kill $pid 2>&1 >> /dev/null
