98e059998803a827c9d268485b6e2b86c8f374d1
Harshil Shah <shaharshil2001@gmail.com>
2021-10-06 13:36:24 +0530

    local commit

--- NAS.sh
+++ NAS.sh
@@ -401,10 +401,10 @@ Backup_func()
 {
     read -p "Enter complete path of the folder you want to backup --> " filename
     read -p "Enter path of directory where you want backup to be created --> " backup_dir
-    echo -e "\nBackup will be created as 'filename.tar.gz' at '$HOME/Desktop/' location\n"
+    echo -e "\nBackup will be created as 'filename.tar' at '$HOME/Desktop/' location\n"
     spin2 "Creating a Compressed backup..." &
     pid=$!
-    tar -cfj $backup_dir/backup-$(date "+%d-%m-%Y_%H-%M-%S").tar.tbz  $filename >> /dev/null
+    tar -cf $backup_dir/backup-$(date "+%d-%m-%Y_%H-%M-%S").tar  $filename &>> /dev/null
     success_command=$?
     sleep 5
     echo -e "\n"
